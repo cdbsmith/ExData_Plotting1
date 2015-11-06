6c9bcb5f2b812b798bf73d54f248a77522365b10
cdbsmith <cdbsmith@hotmail.com>
2015-11-06 05:18:47 -0800

    Peer Assessment 1
    Files for Peer Assessment 1

---
+++ plot1.R
@@ -0,0 +1,21 @@
+# Create variale with file name
+filename <- "exdata-data-household_power_consumption.zip"
+
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+        download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("exdata-data-household_power_consumption")) { 
+        unzip(filename) 
+}
+
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
